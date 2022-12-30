6acbd946092ae6b33f6e2c1d18dea0eac1e41659
heiskwizera <kwizerageniklenon@gmail.com>
2022-12-30 10:23:10 +0200

    ls integration

--- main.js
+++
@@ -1,7 +0,0 @@
-let obj = { name: "John", age: 30 };
-
-// Store the object in local storage
-localStorage.setItem("user", JSON.stringify(obj));
-
-// Retrieve the object from local storage
-let user = JSON.parse(localStorage.getItem("user"));
